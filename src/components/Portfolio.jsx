c717b2d22a19e7e011be7009846088f368f66f5c
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-12-09 21:00:44 +0800

    fix lint issues

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -33,4 +33,4 @@ export default function Portfolio (props) {
       <ProjectList projects={projectFiltered} />
     </>
   )
-};
+}
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -14,4 +14,4 @@ export default function Toolbar( props ) {
       }) }
     </div>
   )
-};
+}
